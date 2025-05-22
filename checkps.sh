4a439d28e2b5b181066e3ed08cee95989b7df1f2
acGHTvYKVY <113487230+acGHTvYKVY@users.noreply.github.com>
2025-05-23 02:29:50 +0800

    Update checkps.sh

--- checkps.sh
+++ checkps.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+PID=`ps aux | grep '/root/executor/executor/bin/executor' | grep -v grep | awk '{print $2}' | wc -l`
 
+echo $PID
+
+#if[ $PID="" ]; then
+if [ "$PID" = "0" ]; then
+  echo "BRN没启动"
+  /root/runBRN.sh
+else
+  echo "BRN启动着"
+fi
